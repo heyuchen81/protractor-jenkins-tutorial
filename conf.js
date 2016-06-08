1044f6f24164077ff9a7f767d66daa6c7b544385
Xin He <Xin.He@ingenta.com>
2016-06-08 11:34:03 +0100

    Commit to run protractor tests in BrowserStack. [change env]

--- conf.js
+++ conf.js
@@ -6,11 +6,19 @@ exports.config = {
   seleniumAddress: 'http://hub.browserstack.com/wd/hub',
   specs: ['spec.js'],
   capabilities: {
-	  'browserName' : 'IE',
-	  'browser_version' : '8.0',
+//    Failed Angular Test In IE 	  
+//	  'browserName' : 'IE',
+//	  'browser_version' : '8.0',
+//	  'os' : 'Windows',
+//	  'os_version' : '7',
+//	  'resolution' : '1024x768',
+	  
+	  'browserName' : 'Chrome',
+	  'browser_version' : '51.0',
 	  'os' : 'Windows',
-	  'os_version' : '7',
-	  'resolution' : '1024x768',
+	  'os_version' : '10',
+	  'resolution' : '1920x1200',
+	  
 	  'browserstack.user' : process.env.USERNAME,
 	  'browserstack.key' : process.env.AUTOMATE_KEY,
 	  'browserstack.debug' : 'true'
