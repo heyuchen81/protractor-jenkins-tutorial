7f9671666f0d523d2c0d0b6e56b444aee69480c0
Xin He <Xin.He@ingenta.com>
2016-06-08 11:24:38 +0100

    Commit to run protractor tests in BrowserStack.

--- conf.js
+++ conf.js
@@ -2,10 +2,18 @@
 //conf.js
 exports.config = {
   framework: 'jasmine',
-  seleniumAddress: 'http://localhost:4444/wd/hub',
+  //seleniumAddress: 'http://localhost:4444/wd/hub',
+  seleniumAddress: 'http://hub.browserstack.com/wd/hub',
   specs: ['spec.js'],
   capabilities: {
-	  browserName: 'firefox'
+	  'browserName' : 'IE',
+	  'browser_version' : '8.0',
+	  'os' : 'Windows',
+	  'os_version' : '7',
+	  'resolution' : '1024x768'
+	  'browserstack.user' : process.env.USERNAME,
+	  'browserstack.key' : process.env.AUTOMATE_KEY,
+	  'browserstack.debug' : 'true'
   }
 //  multiCapabilities: [{
 //	  browserName: 'firefox'
